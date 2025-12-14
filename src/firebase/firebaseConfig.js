70dbf427fdc9e653cd964c32ee3b756e64296ea8
Nehad <nehad.elsayed@robustastudio.com>
2025-12-15 01:10:37 +0200

    edit env and config file

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,19 +6,20 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: import.meta.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
 
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export default app;
+
+
--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import app from "../../firebase/firebaseConfig";
+import { ref, push, set, getDatabase } from "firebase/database";
 
 export default function Write() {
-  return <div>Write</div>;
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
+
+  async function saveData() {
+    const db = getDatabase(app);
+    const newDocRef = push(ref(db, "nature/fruits"));
+    set(newDocRef, {
+      fruitName: inputValue1,
+      fruitDefinition: inputValue2,
+    });
+  }
+
+  return (
+    <>
+      <div style={{ backgroundColor: "beige" }}>
+        <input
+          type="text"
+          value={inputValue1}
+          onChange={(e) => {
+            setInputValue1(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          value={inputValue2}
+          onChange={(e) => {
+            setInputValue2(e.target.value);
+          }}
+        />
+        <button value={saveData}>saveData</button>
+      </div>
+    </>
+  );
 }
