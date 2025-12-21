dfdecd4dbf3f5339ba738487d3713cc1485ec830
Nehad <nehad.elsayed@robustastudio.com>
2025-12-21 20:21:44 +0200

    edit fireConfig

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "my-app-42000",
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
@@ -21,5 +21,3 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export default app;
-
-
